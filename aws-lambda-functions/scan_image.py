a721ce664053969d155f8e860d7d90f066609f92
pimpmfl <danielpopescu934@gmail.com>
2023-12-27 18:05:05 +0100

    feat(scan-image): implemented logic #7

--- aws-lambda-functions/scan_image.py
+++ aws-lambda-functions/scan_image.py
@@ -1,10 +1,41 @@
-import json
+import boto3
 
+# This function takes the bytes string of an image and runs it through Amazon Rekognition to check if there are any faces present
+# Returns a response in JSON
 def lambda_handler(event, context):
-    # TODO base64 encoded image string
-    # run it through Amazon Rekognition
-    # return if there are people's faces in the image
-    return {
-        'presentFaces': True,
-        'base64Image': "someString"
-    }
+    # Setup
+    image = event['img']
+    region = 'us-east-1'
+    service = 'rekognition'
+    # Running this on your local machine will require additional parameters for the AWS credentials
+    rekognition = boto3.client(service, region)
+
+    # Run the image through Amazon Rekognition
+    response = rekognition.detect_faces(
+        Image={'Bytes': image},
+        Attributes=['ALL']
+    )
+
+    # Each face found in the picture has a separate attribute called FaceDetails
+    if response['FaceDetails']:
+        return {
+            'faces_in_image': len(response['FaceDetails']),
+            'image': event['img']
+        }
+    else:
+        return {
+            'faces_in_image': 0,
+            'image': event['img']
+        }
+
+# Simulate event locally    
+#if __name__ == '__main__':
+#    with open('/home/ubuntu/Al_Davis_0001.jpg', 'rb') as image_file:
+#        image_data = image_file.read()
+#    
+#    test_event = {
+#        'img': image_data
+#    }
+#    response = lambda_handler(test_event, None)
+#    print(response)
+    
