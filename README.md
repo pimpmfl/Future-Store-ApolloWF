f01d61608aac6133823d84f29aeb48920eb1d415
Tobias Beiser <tobias.beiser@gmail.com>
2024-01-14 21:52:14 +0100

    improved performance of face mapping

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ The pictures of the customers entering the store are run through Amazon Rekognit
 - [AFCL](https://dps.uibk.ac.at/projects/afcl/subpages/user-documentation/index.html)
 - [AWS](https://aws.amazon.com/)
 - [Python](https://www.python.org/)
+
+
+## Setup:
+
+### EC2:
+Add an inbound TCP rule on port 6379 
+Don't forget to update the IAM role of the instance to LabRole
+
+### Lambda Functions:
+Add layers to functions
+Configure correct VPC for functions that need access to the EC2 Cluster and change the EC2-IP in the code to private IPv4 addresses of EC2 instance
--- aws-lambda-functions/map_face_to_user.py
+++ aws-lambda-functions/map_face_to_user.py
@@ -1,6 +1,50 @@
 import json, redis, boto3, base64
+ec2IP = '44.206.127.26'
+def create_face_collection(rekognition, collection_id):
+    existing_collections=rekognition.list_collections(MaxResults=5).get('CollectionIds')
+    if ((existing_collections is None) or (collection_id not in existing_collections)):
+        rekognition.create_collection(CollectionId=collection_id)
+        return True
+    else:
+        return False
 
-ec2IP = '127.0.0.1'
+def index_faces(rekognition, collection_id, image, key):
+    response = rekognition.index_faces(
+        CollectionId=collection_id,
+        Image={'S3Object': {
+            'Bucket' : image.bucket_name,
+            'Name': image.key
+        }},
+        ExternalImageId=key
+    )
+    return response['FaceRecords']
+
+def search_faces_by_image(rekognition, collection_id, image_bytes):
+    response = rekognition.search_faces_by_image(
+        CollectionId=collection_id,
+        Image={'Bytes': image_bytes}
+    )
+    return response['FaceMatches']
+
+def index_collection(redis, bucket, rekognition, collection_id):
+    print('indexing')
+    for username in redis.keys():
+        #connect to s3 and recieve the images for each user
+        folder_path = 'faces/' + username
+        images_of_user = bucket.objects.filter(Prefix=folder_path)
+        for user_image in images_of_user:
+            start_request = time.time()
+            index_faces(rekognition,collection_id, user_image, username)
+
+def upload_image_to_s3(bucket_name, key, image_bytes):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    bucket.put_object(Key=key, Body=image_bytes)
+    return bucket.Object(key)
+
+def delete_image_from_s3(bucket_name, key):
+    s3 = boto3.client('s3')
+    s3.delete_object(Bucket=bucket_name, Key=key)
 
 def lambda_handler(event, context):
     face64 = event.get('face')
@@ -8,63 +52,54 @@ def lambda_handler(event, context):
         return {
             'error': "No image has been provided."
         }
-
     face = base64.b64decode(face64)
+
+    #S3
+    bucket_name = 'automatedstore'
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+
+    #Rekogition
     region = 'us-east-1'
     service = 'rekognition'
-    # Running this on your local machine will require additional parameters for the AWS credentials
     rekognition = boto3.client(service, region)
-
-    bucket_name = 'automatedstore'
-    s3 = boto3.client('s3')
-   
+    collection_id='currentUsersInDatabase'
+    
+    #Redis
     r = redis.Redis(
         host=ec2IP, port=6379,
         username="default",
         password="Mkmkl0loopo",
-        decode_responses=True
+        decode_responses=True,
     )
+
+
+    # rekognition.delete_collection(CollectionId=collection_id)
+    if create_face_collection(rekognition, collection_id):
+        index_collection(r, bucket, rekognition, collection_id)
+
 
     highest_confidence:float = 0
     best_match:str = '-1'
 
-    print('matching faces...this might take some time')
-    for key in r.keys():
-        #connect to s3 and recieve the images for each user
-        folder_path = 'faces/' + key
-        s3_response = s3.list_objects(Bucket=bucket_name, Prefix=folder_path)
-        if 'Contents' in s3_response:
-            for obj in s3_response['Contents']:
-                object_key = obj['Key']
-                target_image_bytes=s3.get_object(Bucket=bucket_name, Key=object_key)['Body'].read()        
+    search_results = search_faces_by_image(rekognition, collection_id, face)
+    for match in search_results:
+        similarity = match.get('Similarity')
+        if similarity > 80 and similarity > highest_confidence:
+            highest_confidence=similarity
+            best_match = match.get('Face')['ExternalImageId']
 
-                response = rekognition.compare_faces(
-                    SourceImage={'Bytes': face},
-                    TargetImage={'Bytes': target_image_bytes},
-                    SimilarityThreshold= 80
-                )
-                matches=response.get('FaceMatches')
-                for match in matches:
-                    similarity = match.get('Similarity')
-                    if similarity > highest_confidence:
-                        highest_confidence=similarity
-                        best_match = key
-
+    print('Best match:', best_match)
     return {
         'customerId': best_match
     }
 
 
-
-# #Simulate locally
+#Simulate locally
 # if __name__ == '__main__':
 #     with open('response_detect_face.json', 'r') as test_event_data:
 #         json_string = test_event_data.read()
-#         faces = json.loads(json_string).get('faces')
-#         test_event = {
-#             'faces' : faces,
-#             'face': faces[0]
-#         }
-#         response = lambda_handler(test_event,None)
+#         face = json.loads(json_string)
+#         response = lambda_handler(face,None)
 #         with open('response_map_face_to_user.json', 'w') as f:
 #             json.dump(response, f)
