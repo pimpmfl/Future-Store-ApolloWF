1db9c8df9c7713896abd48a2e20f589cfe0bb73a
Tobias Beiser <tobias.beiser@gmail.com>
2024-01-14 21:52:55 +0100

    updated todo

--- aws-lambda-functions/register_new_customer.py
+++ aws-lambda-functions/register_new_customer.py
@@ -2,6 +2,7 @@
 def lambda_handler(event, context):
     # TODO input is a base64 image string containing a face
     # implement logic to save the new customer in the database and return new customerId
+    # also add the new customer to the rekognition collection created in map_face_to_user
     return {
         'customerId': 2
     }
