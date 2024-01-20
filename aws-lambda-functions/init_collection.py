19216731291e346132be26355e469800c9e03f5c
Tobias Beiser <tobias.beiser@gmail.com>
2024-01-20 18:16:40 +0100

    separated map_faces and rekognition collection intialization

---
+++ aws-lambda-functions/init_collection.py
@@ -0,0 +1,63 @@
+import boto3
+import redis
+# IMPORTANT: This function takes a long time to build the initial indecies. 
+# Set timeout for first run to 60+ seconds and reduce afterwards
+
+ec2IP = 'ec2-44-206-127-26.compute-1.amazonaws.com'
+
+def create_face_collection(rekognition, collection_id):
+    existing_collections = rekognition.list_collections(
+        MaxResults=5).get('CollectionIds')
+    if ((existing_collections is None) or (collection_id not in existing_collections)):
+        rekognition.create_collection(CollectionId=collection_id)
+        return True
+    else:
+        return False
+
+def index_faces(rekognition, collection_id, image, key):
+    response = rekognition.index_faces(
+        CollectionId=collection_id,
+        Image={'S3Object': {
+            'Bucket': image.bucket_name,
+            'Name': image.key
+        }},
+        ExternalImageId=key
+    )
+    return response['FaceRecords']
+
+def index_collection(redis, bucket, rekognition, collection_id):
+    print('indexing')
+    for username in redis.keys():
+        # connect to s3 and recieve the images for each user
+        folder_path = 'faces/' + username
+        images_of_user = bucket.objects.filter(Prefix=folder_path)
+        for user_image in images_of_user:
+            index_faces(rekognition, collection_id, user_image, username)
+
+def lambda_handler(event, context):
+    # Rekogition
+    region = 'us-east-1'
+    service = 'rekognition'
+    rekognition = boto3.client(service, region)
+    collection_id = 'currentUsersInDatabase'
+
+    # S3
+    bucket_name = 'automatedstore'
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+
+    # Redis
+    r = redis.Redis(
+        host=ec2IP, port=6379,
+        username="default",
+        password="Mkmkl0loopo",
+        decode_responses=True,
+    )
+
+    rekognition.delete_collection(CollectionId=collection_id)
+    if create_face_collection(rekognition, collection_id):
+        index_collection(r, bucket, rekognition, collection_id)
+
+    return event
+
+
--- aws-lambda-functions/map_face_to_user.py
+++ aws-lambda-functions/map_face_to_user.py
@@ -3,30 +3,29 @@ import redis
 import boto3
 import base64
 import os
-# IMPORTANT: This function takes a long time to build the initial indecies. Set timeout for first run to 60+ seconds and reduce afterwards
 ec2IP = 'ec2-44-206-127-26.compute-1.amazonaws.com'
 
+def get_best_match(search_results):
+    highest_confidence: float = 0
+    best_match: str = '-1'
 
-def create_face_collection(rekognition, collection_id):
-    existing_collections = rekognition.list_collections(
-        MaxResults=5).get('CollectionIds')
-    if ((existing_collections is None) or (collection_id not in existing_collections)):
-        rekognition.create_collection(CollectionId=collection_id)
-        return True
-    else:
-        return False
+    for match in search_results:
+        similarity = match.get('Similarity')
+        if similarity > 80 and similarity > highest_confidence:
+            highest_confidence = similarity
+            best_match = match.get('Face')['ExternalImageId']
+            
+    return best_match
 
 
-def index_faces(rekognition, collection_id, image, key):
-    response = rekognition.index_faces(
-        CollectionId=collection_id,
-        Image={'S3Object': {
-            'Bucket': image.bucket_name,
-            'Name': image.key
-        }},
-        ExternalImageId=key
-    )
-    return response['FaceRecords']
+def remove_face_from_collection(rekognition, collection_id, username):
+    face_ids = []
+    faces = rekognition.list_faces(CollectionId=collection_id)['Faces']
+    for face in faces:
+        if face.get('ExternalImageId') == username:
+            face_ids.append(face['FaceId'])
+
+    rekognition.delete_faces(CollectionId=collection_id, FaceIds=face_ids)
 
 
 def search_faces_by_image(rekognition, collection_id, image_bytes):
@@ -37,16 +36,6 @@ def search_faces_by_image(rekognition, collection_id, image_bytes):
     return response['FaceMatches']
 
 
-def index_collection(redis, bucket, rekognition, collection_id):
-    print('indexing')
-    for username in redis.keys():
-        # connect to s3 and recieve the images for each user
-        folder_path = 'faces/' + username
-        images_of_user = bucket.objects.filter(Prefix=folder_path)
-        for user_image in images_of_user:
-            index_faces(rekognition, collection_id, user_image, username)
-
-
 def lambda_handler(event, context):
     face64 = event.get('face')
     if face64 is None:
@@ -54,10 +43,6 @@ def lambda_handler(event, context):
             'error': "No image has been provided."
         }
     face = base64.b64decode(face64)
-    # S3
-    bucket_name = 'automatedstore'
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucket_name)
 
     # Rekogition
     region = 'us-east-1'
@@ -73,31 +58,13 @@ def lambda_handler(event, context):
         decode_responses=True,
     )
 
-
-#    rekognition.delete_collection(CollectionId=collection_id)
-    if create_face_collection(rekognition, collection_id):
-        index_collection(r, bucket, rekognition, collection_id)
-
-    highest_confidence: float = 0
-    best_match: str = '-1'
-
     search_results = search_faces_by_image(rekognition, collection_id, face)
-    for match in search_results:
-        similarity = match.get('Similarity')
-        if similarity > 80 and similarity > highest_confidence:
-            highest_confidence = similarity
-            best_match = match.get('Face')['ExternalImageId']
+    best_match = get_best_match(search_results)
 
     if (best_match != '-1') and (best_match not in r.keys()):
         print(
             f'user {best_match} not present in database. deleting matched image from collection')
-        face_ids = []
-        faces = rekognition.list_faces(CollectionId=collection_id)['Faces']
-        for face in faces:
-            if face.get('ExternalImageId') == best_match:
-                face_ids.append(face['FaceId'])
-
-        rekognition.delete_faces(CollectionId=collection_id, FaceIds=face_ids)
+        remove_face_from_collection(rekognition, collection_id, best_match)
         best_match = '-1'
 
     print('Best match:', best_match)
